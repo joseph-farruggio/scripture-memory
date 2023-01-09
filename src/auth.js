0753a24bf388ddd8e87ad0f64ee46d0a300fdc6e
Joey Farruggio <joey@joeyfarruggio.com>
2023-01-09 15:31:17 -0600

    wip

--- src/auth.js
+++ src/auth.js
@@ -30,18 +30,16 @@ const { data, error } = await supabase
 
 
 export async function updateCompletedColumn(user, IDs) {
-  // generate uuid in js
-  const uuid = () => {
-    let dt = new Date().getTime();
-    const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      const r = (dt + Math.random()*16)%16 | 0;
-      dt = Math.floor(dt/16);
-      return (c=='x' ? r :(r&0x3|0x8)).toString(16);
-    });
+  function uuid() {
+    let uuid = '';
+    for (let i = 0; i < 32; i++) {
+      uuid += Math.floor(Math.random() * 10);
+    }
+    return uuid;
   }
 
   return await supabase
     .from('reading_progress')
-    .upsert({'id': uuid, 'completed': IDs })
+    .upsert({'id': uuid(), 'completed': IDs })
     .eq('user_id', user.id )
 }
